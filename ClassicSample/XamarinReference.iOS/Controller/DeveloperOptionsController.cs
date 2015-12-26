17bf5d178f65c27fc7eec4e7f6cbcbb686c798a4
Aaron LaBeau <aaronlab@magenic.com>
2015-12-26 18:35:03 -0500

    added files for file logging

---
+++ ClassicSample/XamarinReference.iOS/Controller/DeveloperOptionsController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Foundation;
+using UIKit;
+using XamarinReference.Lib.Interface;
+
+namespace XamarinReference.iOS.Controller
+{
+    public class DeveloperOptionsController : BaseTableViewController
+    {
+        //_backButton is used to display a back button to go back to the AboutController from the NavController
+        private UIBarButtonItem _backButton;
+
+        //_shareViewController - used to show the share view with options to share the log file via various activities
+        private UIActivityViewController _shareViewController;
+
+        public DeveloperOptionsController() { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            SetupUi();
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            SetupBackNavigationButton();
+            base.ViewDidAppear(animated);
+        }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            //currently hard coded to one as all we have is the logging section
+            return 1;
+        }
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            var rows = 0;
+            var rowsLogging = 3;
+
+            switch (section)
+            {
+                //logging section
+                case 0:
+                    rows = rowsLogging; 
+                    break;
+            }
+            return rows;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = new UITableViewCell(UITableViewCellStyle.Default, "LoggingCell")
+            {
+                SelectionStyle = UITableViewCellSelectionStyle.None
+            };
+
+            switch (indexPath.Row)
+            {
+                case 0:
+                    cell.TextLabel.Text = _localizeLookupService.GetLocalizedString("LogFileView");
+                    cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+                    break;
+                case 1:
+                    cell.TextLabel.Text = _localizeLookupService.GetLocalizedString("LogFileShare");
+                    cell.Accessory = UITableViewCellAccessory.None;
+                    break;
+                case 2:
+                    cell.TextLabel.Text = _localizeLookupService.GetLocalizedString("LogFileDelete");
+                    cell.Accessory = UITableViewCellAccessory.None;
+                    break;
+            } 
+            return cell;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            switch (indexPath.Row)
+            {
+                case 0:
+                    ViewLogFile();
+                    break;
+                case 1:
+                    ShareLogFile();
+                    break;
+                case 2:
+                    DeleteLogFile();
+                    break;
+            }
+        }
+
+        
+        public override string TitleForHeader(UITableView tableView, nint section)
+        {
+            var headerLabel = string.Empty;
+            switch (section)
+            {
+                case 0:
+                    headerLabel = _localizeLookupService.GetLocalizedString("DeveloperLogging");
+                    break;
+            }
+
+            return headerLabel;
+        }
+
+        private void ViewLogFile()
+        {
+            //TODO:  show modal popup with the log file contents
+        }
+
+        private async void DeleteLogFile()
+        {
+            //make sure the logging option supports deleting files
+            if (_logging is ILoggingFile)
+            {
+                //confirm via UIAlert to delete
+                var isDelete = await Helper.Utility.ShowAlert(_localizeLookupService.GetLocalizedString("Confirm"), _localizeLookupService.GetLocalizedString("DeleteFileMessage"), _localizeLookupService.GetLocalizedString("Delete"), _localizeLookupService.GetLocalizedString("Cancel"));
+                if (isDelete)
+                {
+                    //delete the file 
+                    ((ILoggingFile)_logging).DeleteLog();
+
+                }
+            }
+        }
+
+        private void ShareLogFile()
+        {
+            try
+            {
+                //make sure the current logging service supports sharing the log file - some logging services shouldn't support this
+                if (_logging is ILoggingFile)
+                {
+                    //get the file path of the log file to share
+                    var path = ((ILoggingFile)_logging).LoggingPath;
+
+                    //setup the view control to show sharing options
+                    _shareViewController = new UIActivityViewController(
+                        new NSObject[] { NSUrl.FromFilename(path) },
+                        null)
+                    {
+                        //used to exclude sharing the log file to these types 
+                        //of activities
+                        ExcludedActivityTypes = new NSString[]
+                        {
+                            UIActivityType.AddToReadingList,
+                            UIActivityType.AssignToContact,
+                            UIActivityType.OpenInIBooks,
+                            UIActivityType.PostToFacebook,
+                            UIActivityType.PostToFlickr,
+                            UIActivityType.PostToTencentWeibo,
+                            UIActivityType.PostToTwitter,
+                            UIActivityType.PostToVimeo,
+                            UIActivityType.PostToWeibo,
+                            UIActivityType.Print,
+                            UIActivityType.SaveToCameraRoll
+                        },
+                    };
+                    this.PresentViewController(_shareViewController, true, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.DealWithErrors(ex);
+            }
+        }
+
+        private void SetupBackNavigationButton()
+        {
+            //setup the back button to go back to the category listing
+            _backButton = new UIBarButtonItem
+            {
+                Image = UIImage.FromBundle("back_white.png"),
+            };
+
+            //handle back button clicked to go back to the About Controller
+            _backButton.Clicked += (o, e) =>
+            {
+                this.NavMenuController.PopViewController(true);
+            };
+
+            this.NavigationItem.SetLeftBarButtonItem(_backButton, false);
+        }
+
+        private void SetupUi()
+        {
+            this.Title = _localizeLookupService.GetLocalizedString("DeveloperOptions");
+
+            SetupTableView();
+        }
+
+        private void SetupTableView()
+        {
+            this.TableView = new UIKit.UITableView(View.Bounds, UIKit.UITableViewStyle.Grouped);
+            this.TableView.SeparatorStyle = UIKit.UITableViewCellSeparatorStyle.SingleLine;
+            this.TableView.AutoresizingMask = UIKit.UIViewAutoresizing.All;
+            this.TableView.RowHeight = UIKit.UITableView.AutomaticDimension;
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (_backButton != null)
+            {
+                _backButton.Dispose();
+                _backButton = null;
+            }
+            if (_shareViewController != null)
+            {
+                _shareViewController.Dispose();
+                _shareViewController = null;
+            }
+        }
+    }
+}
---
+++ ClassicSample/XamarinReference.iOS/Services/FileLoggingService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Cirrious.CrossCore;
+
+using XamarinReference.Lib.Interface;
+using XamarinReference.Lib.Logging;
+
+namespace XamarinReference.iOS
+{
+    public class FileLoggingService : ILoggingService, ILoggingFile
+    {
+        private readonly IFileHelper _fileHelper = Mvx.Resolve<IFileHelper>();
+        private string _loggingPath;
+
+        public string LoggingPath => _loggingPath;
+
+        public FileLoggingService()
+        {
+            _loggingPath = string.Format(@"{0}/logfile.txt", _fileHelper.GetLocalStoragePath);
+        }
+
+        public void WriteLine(MessageType messageType, string message)
+        {
+            var text = string.Empty;
+
+            switch (messageType)
+            {
+                case MessageType.Error:
+                    text = string.Format("{0} ERROR:- {1}. {2}", DateTime.Now, message, System.Environment.NewLine);     
+                    break;
+                case MessageType.FailureAudit:
+                    text = string.Format("{0} FAILURE AUDIT:- {1}. {2}", DateTime.Now, message, System.Environment.NewLine);     
+                    break;
+                case MessageType.Information:
+                    text = string.Format("{0} INFORMATION:- {1}. {2}", DateTime.Now, message, System.Environment.NewLine);     
+                    break;
+                case MessageType.SuccessAudit:
+                    text = string.Format("{0} SUCCESS AUDIT:- {1}. {2}", DateTime.Now, message, System.Environment.NewLine);     
+                    break;
+                case MessageType.Warning:
+                    text = string.Format("{0} WARNING:- {1}. {2}", DateTime.Now, message, System.Environment.NewLine);     
+                    break;
+            }
+
+            //write information to log file
+            _fileHelper.AppendAllTextContent(_loggingPath, text);
+        }
+
+        public void DeleteLog()
+        {
+            _fileHelper.Delete(_loggingPath);
+            this.WriteLine(MessageType.Information, string.Format("Logfile was deleted and recreated on {0}", DateTime.Now));
+        }
+    }
+}
