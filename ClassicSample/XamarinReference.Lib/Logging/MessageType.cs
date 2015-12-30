f884e82f1e1bf436edb353ead0aa040370049b2a
Aaron LaBeau <aaronlab@magenic.com>
2015-12-30 15:25:06 -0500

    added missing MessageType class

---
+++ ClassicSample/XamarinReference.Lib/Logging/MessageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinReference.Lib.Logging
+{
+    public enum MessageType
+    {
+        Information,
+        Warning,
+        Error,
+        SuccessAudit,
+        FailureAudit
+    };
+}
